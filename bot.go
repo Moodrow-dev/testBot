3acaa80e89209168675cf9db83db9d78d7572b91
moodrow <makarov_a_v@sc.vsu.ru>
2025-12-16 11:07:05 +0300

    Boosty trigger replaced to telega Add reply to messages

--- bot.go
+++ bot.go
@@ -304,7 +304,7 @@ func AdvertiseTelega(bh *th.BotHandler, db *sql.DB) {
 		if chat == nil {
 			return nil
 		}
-		bot.SendMessage(ctx, &telego.SendMessageParams{ReplyParameters: &telego.ReplyParameters{MessageID: update.Message.MessageID}, LinkPreviewOptions: &telego.LinkPreviewOptions{IsDisabled: true}, ChatID: chatID, ParseMode: telego.ModeMarkdownV2, DisableNotification: true, Text: "[Ссылка на телегу](https://t.me/starsresearchteam)"})
+		bot.SendMessage(ctx, &telego.SendMessageParams{ReplyParameters: &telego.ReplyParameters{MessageID: update.Message.MessageID}, LinkPreviewOptions: &telego.LinkPreviewOptions{IsDisabled: true}, ChatID: chatID, ParseMode: telego.ModeMarkdownV2, DisableNotification: true, Text: "[Ссылка на телегу](https://t.me/starsresearchnews)"})
 		return nil
 	}, th.TextMatches(regexp.MustCompile(`(?:^|\s|[.,!?])([Тт][Ее][Лл][Ее][Гг]([Аа]|[Ии]|[Ее]|[Уу]|[Oo][Йй]))(?:\s|[.,!?]|$)`)))
 }
